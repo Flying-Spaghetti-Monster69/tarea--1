186378cf09c08623dad185607a043681de278ffe
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-08-29 09:44:47 -0500

    ejercicio 12 completo

---
+++ ejercicio 12.py
@@ -0,0 +1,9 @@
+horas = 48
+valorHoras = 5000
+retencion = 0.125
+
+salarioBruto = horas * valorHoras
+retencionValor = salarioBruto * retencion
+salarioNeto = salarioBruto - retencionValor
+
+print("el salario bruto es:",salarioBruto,"\nel salario neto es:",salarioNeto,"\nla retencion es:",retencionValor)
