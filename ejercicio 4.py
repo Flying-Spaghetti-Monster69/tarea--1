1d5f874444b8d8bad0f7daed03aa0eb0e5abd63c
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-08-29 08:47:21 -0500

    ejercicio 4 terminado

---
+++ ejercicio 4.py
@@ -0,0 +1,6 @@
+edadJuan = int(input("entre la edad de juan: "))
+edadAlberto = edadJuan * (2/3)
+edadAna = edadJuan * (4/3)
+edadMama = edadJuan + edadAlberto + edadAna
+
+print("edad de Juan:",edadJuan,"\nedad de alberto:",edadAlberto,"\nedad de ana:",edadAna,"\nedad de la mama:",edadMama)
