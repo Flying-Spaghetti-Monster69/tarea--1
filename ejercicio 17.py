25ba5cb06700f713d9b4cab7fba99b04563ba20c
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-08-29 09:31:58 -0500

    Ejercicio 17 completo

---
+++ ejercicio 17.py
@@ -0,0 +1,6 @@
+import math
+
+# el pi esta aproximado a 15 decimales
+
+radio = float(input("escriba el radio del circulo: "))
+print("el area del circulo es:",math.pi*(radio**2),"\nla circuferencia del circulo es:",2*math.pi*radio)
